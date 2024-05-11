feaadebe20fc0749b5c2e9b723b5a50ae6331cc4
cubic <cubicgraphics98@gmail.com>
2024-05-11 16:36:10 +0100

    Add project files.

---
+++ BeatTogether.UnifiedServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using BeatTogether.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using BeatTogether.MasterServer.NodeController.HttpControllers;
+using BeatTogether.MasterServer.Api.HttpControllers;
+using System.Reflection;
+
+
+namespace BeatTogether.MasterServer
+{
+    public static class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureAPIServices().UseMasterServerNodeController().UseMasterServerApi();
+
+        public static IHostBuilder ConfigureAPIServices(this IHostBuilder hostBuilder) =>
+            hostBuilder.ConfigureServices(services => services.AddControllers()
+                .AddApplicationPart(Assembly.GetAssembly(typeof(MasterServerController)))
+                .AddApplicationPart(Assembly.GetAssembly(typeof(GetMultiplayerInstanceController)))
+                .AddControllersAsServices()
+            );
+    }
+}
