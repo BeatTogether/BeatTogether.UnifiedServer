8153b319df410b2f4d4b20716ea3a9444f8bc128
cubic <cubicgraphics98@gmail.com>
2024-05-11 19:36:51 +0100

    Api server is now also loaded

--- BeatTogether.UnifiedServer/Program.cs
+++ BeatTogether.UnifiedServer/Program.cs
@@ -1,19 +1,31 @@
 ﻿using BeatTogether.Extensions;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using BeatTogether.DedicatedServer.Instancing.Extensions;
-
+using BeatTogether.MasterServer.Api.HttpControllers;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using BeatTogether.Status.Api;
+using BeatTogether.Status.Api.Controllers;
 
 namespace BeatTogether.UnifiedServer
 {
     public static class Program
     {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            CreateUnifiedServer(args).Build().Run();
+        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args).UseDedicatedServerInstancing().UseMasterServerApi();
+        public static IHostBuilder CreateUnifiedServer(string[] args) =>
+           Host.CreateDefaultBuilder(args).ConfigureAPIServices().UseStatusServer().UseDedicatedServerInstancing().UseMasterServerApi();
 
+        public static IHostBuilder ConfigureAPIServices(this IHostBuilder hostBuilder) =>
+            hostBuilder.ConfigureServices(services => services.AddControllers()
+                .AddApplicationPart(Assembly.GetAssembly(typeof(QuickplayController)))
+                .AddApplicationPart(Assembly.GetAssembly(typeof(StatusController)))
+                .AddApplicationPart(Assembly.GetAssembly(typeof(GetMultiplayerInstanceController)))
+                .AddControllersAsServices()
+        );
     }
 }
